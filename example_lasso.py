1b20350a2052902d74c530dffbc4f4c41fd288e2
Emanuel Laude <emanuellaude@gmail.com>
2024-06-04 17:56:54 +0200

    Commented example

--- example_lasso.py
+++ example_lasso.py
@@ -4,6 +4,9 @@ import convex_optim.function as fun
 import convex_optim.optimizer_base as opt_base
 import convex_optim.optimizer_lipschitz as opt_lip
 
+##
+# minimize 1/2|Ax - b|^2 + lamb |x|_1
+##
 m, n = 300, 400
 np.random.seed(12)
 A = 2 * np.random.rand(m, n) - 1
@@ -12,6 +15,8 @@ lamb = 0.015
 
 x0 = np.random.rand(n)
 
+# setup problem define splitting
+#min_x f(x) + g(x)
 f = fun.AffineCompositeLoss(
             fun.NormPower(2, 2),
             fun.LinearTransform(A),
@@ -24,6 +29,7 @@ g = fun.FunctionTransform(
 problem = opt_base.CompositeOptimizationProblem(x0, f, g)
 
 
+#setup optimizer
 params = opt_lip.Parameters()
 params.maxit = 12000
 params.tol = 1e-13
@@ -32,6 +38,7 @@ params.backtracking = False
 params.epsilon = 1e-12
 params.gamma_init = -1
 
+#collect objective values for plotting within callback
 def callback(x, status):
     objective_values.append(problem.eval_objective(x))
     print("k", status.nit, "objective", problem.eval_objective(x), "residual", status.res, "gamma", status.gamma, "tau", status.tau)
@@ -49,7 +56,7 @@ params.backtracking = True
 params.epsilon = 1e-15
 params.gamma_init = 100
 
-
+# plot suboptimality
 plt.semilogy(np.array(objective_values) - objective_values[-1], label="$F(x) - F^*$")
 plt.legend()
 plt.show()
